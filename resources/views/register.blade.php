ee2155f224c49c382c12154a0e820bbdc92ef918
MatheusSuzart <mathsuzart@outlook.com>
2022-09-30 22:07:08 -0300

    component-register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,6 +4,24 @@
         Register
     </a>
 </x-slot:btn>
-
+<form action="">
+<x-form.text_input
+    type="name"
+    name="name"
+    label="YOUR EMAIL"
+    placeholder="YOUR NAME"
+    />
+    <x-form.text_input
+    type="email"
+    name="email"
+    label="YOUR EMAIL"
+    placeholder="YOUR EMAIL"
+    />
+    <x-form.text_input
+    type="password"
+    name="password"
+    label="YOUR password"
+    />
+</form>
 <h2>Tela de Login</h2>
 </x-layout>
