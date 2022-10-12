c36aab654b92e0ac4b246e991e506e64869f2b56
MatheusSuzart <mathsuzart@outlook.com>
2022-10-11 23:58:12 -0300

    login-page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,4 +8,21 @@
 <h2>Tela de Login</h2>
 
 <a href="{{route('home')}}">HOME</a>
+<form method="POST" action="{{route('task.create.action')}}">
+    <h1>REGISTER</h1>
+    @csrf
+<x-form.text_input
+    type="name"
+    name="name"
+    label="YOUR EMAIL"
+    placeholder="YOUR NAME"
+    />
+ 
+    <x-form.text_input
+    type="password"
+    name="password"
+    label="YOUR password"
+    />
+</form>
+<h2>Tela de Login</h2>
 </x-layout>
